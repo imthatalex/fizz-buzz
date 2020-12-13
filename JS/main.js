5929741620ed06b629f5ce2c15ee4bf03ee29803
imthatalex <hernandezalex8765@gmail.com>
2020-12-12 21:42:49 -0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+function FizzBuzz(counter, fizz, buzz){
+    for(let i=1;i<counter+1;i++){
+        if (!(i % fizz == 0 || i % buzz == 0)) console.log(i);
+        if (i % fizz == 0 && i % buzz == 0) console.log(i + ' ' +  "FizzBuzz");
+        if (i % fizz == 0) console.log(i + ' ' +  "Fizz");
+        if (i % buzz == 0) console.log(i + ' ' +  "Buzz");
+    }
+}
+
+
+console.log(testSomething(5,4));
