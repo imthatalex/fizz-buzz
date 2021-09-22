ee4e1b460c35e6d0696c9fd02551d79aa06e796f
imthatalex <hernandezalex8765@gmail.com>
2021-09-22 15:16:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function FizzBuzz(counter, fizz, buzz){
+function fizzBuzz(counter, fizz, buzz){
     for(let i=1;i<counter+1;i++){
         if (!(i % fizz == 0 || i % buzz == 0)) console.log(i);
         if (i % fizz == 0 && i % buzz == 0) console.log(i + ' ' +  "FizzBuzz");
