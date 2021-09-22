9abe30ca7a7effa5024917ed4f4d601424f7b5ac
imthatalex <hernandezalex8765@gmail.com>
2021-09-21 20:43:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function CustomFizzBuzz(counter, fizz, buzz){
+function FizzBuzz(counter, fizz, buzz){
     for(let i=1;i<counter+1;i++){
         if (!(i % fizz == 0 || i % buzz == 0)) console.log(i);
         if (i % fizz == 0 && i % buzz == 0) console.log(i + ' ' +  "FizzBuzz");
@@ -7,14 +7,4 @@ function CustomFizzBuzz(counter, fizz, buzz){
     }
 }
 
-function OddEvenFizzBuzz(counter){
-    for(let i = 1; i < counter + 1; i++){
-        if(i % 2 == 0){
-            console.log(i + ' ' + 'Fizz');
-        }
-        else{
-            console.log(i + ' ' + 'Buzz');
-        }
-    }
-}
 
